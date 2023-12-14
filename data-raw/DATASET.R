ea5f709473c0f1935327fff525623ce47f07e566
ChristopherNClark <chrclark@fiu.edu>
2023-12-14 13:05:39 -0500

    Initial commit

---
+++ data-raw/DATASET.R
@@ -0,0 +1,35 @@
+#' DATASET
+#'
+#' A dataset containing information for HPS week 1 and week 2.
+#'
+#'
+#' @format
+#' A data frame with ...
+#'
+#' @source
+#' Downloaded from GitHub repository: https://github.com/ChristopherNClark/hpsropn
+#'
+#' @export
+
+#' Prepare and save the DATASET dataset
+#'
+#' @noRd
+#'
+
+# Load necessary libraries
+#' @importFrom arrow read_parquet
+#' @importFrom usethis use_data
+
+
+# Week 1:
+# Read the Parquet file into a data frame
+# Testing
+data_week1 <- arrow::read_parquet("data-raw/HPS_week1_data_parquet.parquet")
+usethis::use_data(data_week1, overwrite = TRUE)
+save(data_week1, file = "data/data_week1")
+
+# Week 2:
+# Read the Parquet file into a data frame
+data_week2 <- arrow::read_parquet("data-raw/HPS_week2_data_parquet.parquet")
+usethis::use_data(data_week2, overwrite = TRUE)
+save(data_week2, file = "data/data_week2")
